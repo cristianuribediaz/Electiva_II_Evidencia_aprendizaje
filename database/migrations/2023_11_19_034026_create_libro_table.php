39c80bdb00e492b4fb19382491d0896ca574adc1
cristianuribediaz <curibe@concejodemedellin.gov.co>
2023-11-19 09:33:04 -0500

    Instalación Laravel V.1

---
+++ database/migrations/2023_11_19_034026_create_libro_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libro', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('Codigo');
+            $table->string('Titulo',100);
+            $table->string('ISBN');
+            $table->string('Editorial');
+            $table->string('Paginas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libro');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\autorController;
+use App\Http\Controllers\ejemplarController;
+use App\Http\Controllers\libroController;
+use App\Http\Controllers\usuarioController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,6 +29,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/dashboard/autor',autorController::class);
+    Route::resource('/dashboard/ejemplar',ejemplarController::class);
+    Route::resource('/dashboard/libro',libroController::class);
+    Route::resource('/dashboard/usuario',usuarioController::class);
 });
 
 require __DIR__.'/auth.php';
